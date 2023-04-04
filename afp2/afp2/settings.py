a5f5928da69e2f43d4dcf20de3fe3dca063ff64e
fulopmartin21 <fulop.martin21@gmail.com>
2023-04-04 17:44:53 +0200

    feat: created mysql database connection string and implemented it

--- afp2/afp2/settings.py
+++ afp2/afp2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'afp2'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'afp2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'QUIZ',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+     }
 }
 
 
