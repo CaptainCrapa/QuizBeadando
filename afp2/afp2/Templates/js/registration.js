af96dcb7ca640a7a0d5e590e4450e72628f62bb3
TNorb <teme383@gmail.com>
2023-05-08 13:53:12 +0200

    js verifier of registration

--- afp2/afp2/Templates/js/registration.js
+++ afp2/afp2/Templates/js/registration.js
@@ -7,4 +7,64 @@ function registrationValidation() {
     let dateOfBirth = document.querySelector("#dateOfBirth").value;
     let areFieldsValid = true;
   
+// fullname length check
+  if (fullname.length <= 0)
+  {
+
   }
+  else
+  {
+
+  }
+
+  // username length check
+  if (username.length <= 0)
+  {
+
+  }
+  else
+  {
+
+  }
+
+  // email validation
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email))
+  {
+
+  }
+  else
+  {
+
+  }
+
+  // password length check
+  if (password.length <= 0) 
+  {
+
+  }
+  else
+  {
+
+  }
+
+  // password confirmation check
+  if (confirmPassword != password) 
+  {
+
+  }
+  else
+  {
+
+  }
+
+  // date of birth validation
+  if (!dateOfBirth)
+  {
+
+  }
+  else
+  {
+
+  }
+}
