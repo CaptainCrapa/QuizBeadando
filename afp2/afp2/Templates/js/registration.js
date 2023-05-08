054c6a1b0f35d4923040d397dfb48d20f930e44a
TNorb <teme383@gmail.com>
2023-05-08 13:46:46 +0200

    js base of registration

---
+++ afp2/afp2/Templates/js/registration.js
@@ -0,0 +1,10 @@
+function registrationValidation() {
+    let fullname = document.querySelector("#fullname").value;
+    let username = document.querySelector("#username").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirm-password").value;
+    let dateOfBirth = document.querySelector("#dateOfBirth").value;
+    let areFieldsValid = true;
+  
+  }
