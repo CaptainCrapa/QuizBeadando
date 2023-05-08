9dab13a10f23fafa27bd26293cac43e7d40197eb
TNorb <teme383@gmail.com>
2023-05-08 15:46:45 +0200

    quiz html, js generate + fixes

---
+++ afp2/afp2/Templates/js/quiz.js
@@ -0,0 +1,19 @@
+var role = 'diak';
+var qgenerateLink = document.getElementById("qgenerate");
+var qdeleteLink = document.getElementById("qdelete");
+
+if (role == 'tanar')
+{
+  qgenerateLink.style.display = "visible";
+  qdeleteLink.style.display = "none";
+}
+else if (role == 'adminisztrator')
+{
+  qdeleteLink.style.display = "visible";
+  qgenerateLink.style.display = "none";
+}
+else
+{
+  qdeleteLink.style.display = "none";
+  qgenerateLink.style.display = "none";
+}
